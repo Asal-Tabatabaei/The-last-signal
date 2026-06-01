6742ebf8014845507bfbbd62854d3bcc67b9fd54
Esi8-b <mo.ehsan.gha1384@gmail.com>
2026-06-01 10:15:54 +0330

    Bring simulated_annealing.py from Ehsan branch

--- search/simulated_annealing.py
+++ search/simulated_annealing.py
@@ -26,6 +26,33 @@ class SimulatedAnnealing(LocalSearchBase):
 
         T = initial_temp
 
-        
+        for iteration in range(max_iterations):
+            if T < min_temp:
+                break
 
+            neighbor = self.get_neighbor(current_state)
+            neighbor_cost = self.evaluate(neighbor)
 
+            delta_e = neighbor_cost - current_cost
+
+            if delta_e < 0:
+                current_state = neighbor
+                current_cost = neighbor_cost
+
+                if current_cost < best_cost:
+                    best_state = current_state
+                    best_cost = current_cost
+
+            else:
+                acceptance_probability = math.exp(-delta_e / T)
+
+                if random.random() < acceptance_probability:
+
+                    current_state = neighbor
+                    current_cost = neighbor_cost
+
+            evaluations.append(current_cost)
+            states_history.append(current_state)
+
+            T *= cooling_rate
+        return best_state, best_cost, evaluations, states_history
